15759b7a6269c832f3cbf5f9865d274967216998
Ivancho <ifmm87@gmail.com>
2020-01-12 23:51:49 -0400

    plagiando

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,25 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+//   return str
+//           .split('')
+//           .reverse()
+//           .join('');
+// }
+function reverse(str) {
+  let reversed = '';
+  for(let char of str) {
+    reversed = char + reversed;
+    debugger;
+  }
+  return reversed;
+}
 
+// function reverse(str) {
+//   debugger;
+//   return str.split('').reduce((reversed, character) => character + reversed);
+// }
+let res = reverse('ivanmujica');
+console.log(res);
 module.exports = reverse;
