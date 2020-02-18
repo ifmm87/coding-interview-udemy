dfb9610613b8381d1049b297ad718043884efc25
ivan <ifmm87@gmail.com>
2020-02-17 22:07:00 -0400

    midpoint

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -44,6 +44,98 @@ class LinkedList {
   clear () {
     this.head = null;
   }
+  removeFirst() {
+    if (!this.head) {
+      return;
+    }
+    this.head = this.head.next;
+  }
+  removeLast() {
+    if (!this.head) {
+      return;
+    }
+    let previous = this.head;
+    let node = this.head.next;
+    if (!node){
+      this.head = null;
+      return;
+    }
+    while (node.next) {
+      previous = node;
+      node = node.next;
+    }
+    previous.next = null;
+
+  }
+  insertLast(data) {
+    const lastNode = this.getLast();
+    const newNode = new Node(data);
+    if (lastNode) {
+      lastNode.next = newNode;
+    } else {
+      this.head = newNode;
+    }
+
+  }
+  getAt(index) {
+    if (!this.head) {
+      return null;
+    }
+
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+      counter++;
+      node = node.next;
+    }
+    return null;
+  }
+  removeAt (index) {
+    if (!this.head) {
+      return;
+    }
+    if (index === 0){
+      this.head = this.head.next;
+      return;
+    }
+    const previous = this.getAt(index - 1);
+    if (!previous || !previous.next) {
+      return;
+    }
+    previous.next = previous.next.next;
+  }
+  insertAt(data,  index) {
+    if(!this.head) {
+      this.head = new Node(data);
+    }
+    if(index === 0){
+      this.head = new Node(data, this.head);
+      return;
+    }
+    const previous = this.getAt(index -1) || this.getLast();
+    const node = new Node(data, previous.next);
+    previous.next = node;
+  }
+  forEach(func) {
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+      func(node, counter);
+      node = node.next;
+      counter ++;
+    }
+  }
+  *[Symbol.iterator] ()
+  {
+    let node = this.head;
+    while(node){
+      yield node;
+      node = node.next;
+    }
+  }
 }
 
 module.exports = { Node, LinkedList };
